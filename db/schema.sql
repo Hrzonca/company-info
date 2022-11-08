00c73696b89393d36e8a9d0e709b79723a6264be
Haleigh Rzonca <hgielah@gmail.com>
2022-11-07 18:03:38 -0800

    figured out the table connections

--- db/schema.sql
+++ db/schema.sql
@@ -7,32 +7,26 @@ USE thecompany_db;
 -- connect department id to role department_id, 
 -- connect role id to employee role_id
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     dep_name VARCHAR(30),
-   
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT PRIMARY KEY  AUTO_INCREMENT,
     title VARCHAR(30) INT NOT NULL,
     salary DECIMAL INT NOT NULL,
     dep_id INT NOT NULL
-    FOREIGN KEY(dep_id)
-    REFERENCES department(dep_id)
-    ON DELETE SET NULL 
+    FOREIGN KEY(dep_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    FOREIGN KEY(role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL
-   
-
+    manager_id INT,
+    FOREIGN KEY(role_id) REFERENCES role(id)
+    FOREIGN key (manager_id) REFERENCES role(id)
 );
 -- not sure about the primary key part to connect two parts in the same table. --
 -- look at lesson 20 for primary vs foregin keys --
